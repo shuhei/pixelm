5ee15ed266b403b23a6d8971a1eabf1020aebf86
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2017-09-05 00:24:49 +0200

    Use List instead of Array for History

--- src/History.elm
+++ src/History.elm
@@ -1,25 +1,23 @@
 module History exposing (History, initialize, push, undo, redo)
 
-import Array.Hamt as Array exposing (Array)
-
 
 type History a
-    = History Int (Array a) (Array a)
+    = History Int (List a) (List a)
 
 
 initialize : Int -> History a
 initialize size =
-    History size Array.empty Array.empty
+    History size [] []
 
 
 sameAsHead : a -> History a -> Bool
 sameAsHead a (History _ previous next) =
-    case Array.get 0 previous of
-        Nothing ->
+    case previous of
+        [] ->
             False
 
-        Just head ->
-            head == a
+        x :: _ ->
+            x == a
 
 
 push : a -> History a -> History a
@@ -29,41 +27,30 @@ push item ((History size previous next) as history) =
             if sameAsHead item history then
                 previous
             else
-                Array.append (Array.repeat 1 item) previous
-                    |> Array.slice 0 size
+                List.take size <| item :: previous
     in
-        History size nextPrevious Array.empty
+        History size nextPrevious []
 
 
 undo : a -> History a -> ( Maybe a, History a )
 undo item ((History size previous next) as history) =
-    case Array.get 0 previous of
-        Nothing ->
+    case previous of
+        [] ->
             ( Nothing, history )
 
-        Just popped ->
-            let
-                nextHistory =
-                    History
-                        size
-                        (Array.slice 1 (Array.length previous) previous)
-                        (Array.append (Array.repeat 1 item) next)
-            in
-                ( Just popped, nextHistory )
+        x :: xs ->
+            ( Just x
+            , History size xs (item :: next)
+            )
 
 
 redo : a -> History a -> ( Maybe a, History a )
 redo item ((History size previous next) as history) =
-    case Array.get 0 next of
-        Nothing ->
+    case next of
+        [] ->
             ( Nothing, history )
 
-        Just popped ->
-            let
-                nextHistory =
-                    History
-                        size
-                        (Array.append (Array.repeat 1 item) previous)
-                        (Array.slice 1 (Array.length next) next)
-            in
-                ( Just popped, nextHistory )
+        x :: xs ->
+            ( Just x
+            , History size (item :: previous) xs
+            )
