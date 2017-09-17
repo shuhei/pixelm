4b35fd83899643a283fd5434149b96a1b1aaa9bb
Shuhei Kagawa <shuhei.kagawa@gmail.com>
2017-09-18 00:33:56 +0200

    Fix broken tests

--- tests/HistoryTests.elm
+++ tests/HistoryTests.elm
@@ -18,23 +18,23 @@ all =
                             |> History.push 3
 
                     ( popped1, history1 ) =
-                        History.undo 4 history
+                        History.undo 4 history |> Maybe.withDefault ( 0, history )
 
                     ( popped2, history2 ) =
-                        History.undo 3 history1
+                        History.undo 3 history1 |> Maybe.withDefault ( 0, history1 )
 
-                    ( popped3, history3 ) =
+                    result3 =
                         History.undo 2 history2
 
                     ( popped4, history4 ) =
-                        History.redo 2 history3
+                        History.redo 2 history2 |> Maybe.withDefault ( 0, history2 )
 
                     ( popped5, history5 ) =
-                        History.redo 3 history4
+                        History.redo 3 history4 |> Maybe.withDefault ( 0, history4 )
                 in
                     Expect.equal
-                        ( popped1, popped2, popped3, popped4, popped5 )
-                        ( Just 3, Just 2, Nothing, Just 3, Just 4 )
+                        ( popped1, popped2, result3, popped4, popped5 )
+                        ( 3, 2, Nothing, 3, 4 )
         , test "ignores same item" <|
             \() ->
                 let
@@ -44,18 +44,18 @@ all =
                             |> History.push 1
 
                     ( popped1, history1 ) =
-                        History.undo 2 history
+                        History.undo 2 history |> Maybe.withDefault ( 0, history )
 
-                    ( popped2, history2 ) =
+                    result2 =
                         History.undo 1 history1
 
                     ( popped3, history3 ) =
-                        History.redo 1 history2
+                        History.redo 1 history1 |> Maybe.withDefault ( 0, history1 )
 
-                    ( popped4, history4 ) =
+                    result4 =
                         History.redo 2 history3
                 in
                     Expect.equal
-                        ( popped1, popped2, popped3, popped4 )
-                        ( Just 1, Nothing, Just 2, Nothing )
+                        ( popped1, result2, popped3, result4 )
+                        ( 1, Nothing, 2, Nothing )
         ]
